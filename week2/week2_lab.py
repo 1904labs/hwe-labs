9ff619b8b931437a26f48390a0d0167b82ed41ed
Tim Sagona <tsagona@1904labs.com>
2023-07-18 15:45:15 -0500

    Adding a join to week2 lab

--- week2/week2_lab.py
+++ week2/week2_lab.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import desc
+from pyspark.sql.functions import desc, col
 from pyspark.sql.functions import current_timestamp
 
 
@@ -7,42 +7,42 @@ from pyspark.sql.functions import current_timestamp
 spark = SparkSession.builder.appName("Week2Lab").getOrCreate()
 
 #Question 1: Read the tab separated file named "resources/reviews.tsv.gz" into a dataframe.
-#You will use the "df" dataframe defined here to answer all the questions below...
-df = spark.read.csv("resources/reviews.tsv.gz", sep="\t", header=True)
+#You will use the "reviews" dataframe defined here to answer all the questions below...
+reviews = spark.read.csv("resources/reviews.tsv.gz", sep="\t", header=True)
 
 #Question 2: Display the schema of the dataframe.
-df.printSchema()
+reviews.printSchema()
 
 #Question 3: How many records are in the dataframe? 
 #Store this number in a variable named "count".
-count = df.count()
-print(f"Number of records is {count}")
+reviews_count = reviews.count()
+print(f"Number of records is {reviews_count}")
 
 #Question 4: Print the first 5 rows of the dataframe. 
 #Some of the columns are long - print the entire record, regardless of length.
-df.show(n=5, truncate=False)
+reviews.show(n=5, truncate=False)
 
 #Question 5: Create a new dataframe based on "df" with exactly 1 column: the value of the product category field.
 #Look at the first 50 rows of that dataframe. 
 #Which value appears to be the most common?
-just_product_category = df.select("product_category")
+just_product_category = reviews.select("product_category")
 just_product_category.show(n=50)
 
 #Question 6: Find the most helpful review in the dataframe - the one with the highest number of helpful votes.
 #What is the product title for that review?
-product_title_and_votes = df.sort(desc("helpful_votes")).select("product_title", "helpful_votes")
+product_title_and_votes = reviews.sort(desc("helpful_votes")).select("product_title", "helpful_votes")
 #Either is fine:
 product_title_and_votes.show(n=1, truncate=False)
 #or
 print(product_title_and_votes.first())
 
 #Question 7: How many reviews exist in the dataframe with a 5 star rating?
-five_star_reviews = df.filter(df.star_rating == "5").count()
+five_star_reviews = reviews.filter(reviews.star_rating == "5").count()
 print(f"Number of five star reviews = {five_star_reviews}")
 
 #Question 8: Find the date with the most reviews written.
 #Print the date and total count of the date where the most reviews were written
-review_date_and_count = df.groupBy("review_date").count().sort(desc("count"))
+review_date_and_count = reviews.groupBy("review_date").count().sort(desc("count"))
 #Either is fine:
 review_date_and_count.show(n=1, truncate=False)
 #or:
@@ -51,8 +51,16 @@ print(review_date_and_count.first())
 
 #Question 9: Add a column to the dataframe named "load_timestamp", representing the current time on your computer. 
 #Print the schema and inspect a few rows of data to make sure the data is correctly populated.
-with_load_timestamp = df.withColumn("load_timestamp", current_timestamp())
+with_load_timestamp = reviews.withColumn("load_timestamp", current_timestamp())
 with_load_timestamp.printSchema()
 
+#Question 10: Read the tab separated file named "resources/customers.tsv.gz" into a dataframe.
+#Join it to the reviews dataframe using the common key of customer_id.
+#Create a new dataframe containing the customer_name, date_of_birth, product_title and review_date.
+#Take a look at a few rows to make sure it looks correct.
+customers = spark.read.csv("resources/customers.tsv.gz", sep="\t", header=True)
+
+inner_joined = reviews.join(customers, "customer_id", "inner").select("customer_name", "date_of_birth", "product_title", "review_date")
+inner_joined.show()
 # Stop the SparkSession
 spark.stop()
