944c1e60f86a8fe353cfb9fe942628aac6e0a170
Tim Sagona <tsagona@1904labs.com>
2023-07-24 13:06:12 -0500

    Adding programs to demonstrate how to run 2 programs simultaneously in VS Code

---
+++ week1/first_parallel_program.py
@@ -0,0 +1,25 @@
+import time
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructType, StructField
+
+# Create a SparkSession
+spark = SparkSession.builder.master("local[1]").appName("SparkInstallationTest").getOrCreate()
+
+# Define the data
+data = [("Congratulations! The first Spark program is executing!",),("(This program will run untl you terminate it...)",)]
+
+# Define the schema for the DataFrame
+#schema = StringType()
+schema  = StructType([
+    StructField("message", StringType(), True)
+])
+
+# Create a DataFrames
+df = spark.createDataFrame(data, schema)
+
+# Print the DataFrame
+df.show(truncate=False)
+
+time.sleep(9999)
+# Stop the SparkSession
+spark.stop()
