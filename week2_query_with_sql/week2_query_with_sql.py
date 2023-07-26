b18069a10da920ed3baf06154e74bf8cf922bae9
Tim Sagona <tsagona@1904labs.com>
2023-07-26 11:04:07 -0500

    Fixing week 2 and 3

--- week2_query_with_sql/week2_query_with_sql.py
+++ week2_query_with_sql/week2_query_with_sql.py
@@ -1,23 +1,11 @@
-import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import desc
 from pyspark.sql.functions import current_timestamp
 
-
-aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-aws_session_token = os.environ.get("AWS_SESSION_TOKEN")
-
-
 # Create a SparkSession
 spark = SparkSession.builder \
-    .appName("Week3Lab") \
+    .appName("Week2Lab") \
     .config("spark.sql.shuffle.partitions", "3") \
-    .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider') \
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
-    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
-    .config("spark.hadoop.fs.s3a.session.token", aws_session_token) \
     .config("spark.sql.catalogImplementation", "hive") \
     .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3,org.apache.hadoop:hadoop-aws:3.2.0,com.amazonaws:aws-java-sdk-bundle:1.11.375') \
     .enableHiveSupport()\
@@ -31,12 +19,7 @@ reviews = spark.read.csv("resources/reviews.tsv.gz", sep="\t", header=True)
 with_review_timestamp = reviews.withColumn("review_timestamp", current_timestamp())
 with_review_timestamp.printSchema()
 
-#Question 3: Write the dataframe with load timestamp to s3a://hwe-HANDLE/bronze/reviews in Parquet format.
-with_review_timestamp.write \
-   .mode("overwrite") \
-   .parquet("s3a://hwe-tsagona/bronze/reviews/")
-
-#Question 4: Create a table on top of the reviews directory matchiing the schema from the file.
+#Question 3: Create a table on top of the reviews directory matchiing the schema from the file.
 with_review_timestamp.createOrReplaceTempView("reviews")
 
 #Answer all questions below using Spark SQL instead of the Python API.
@@ -46,31 +29,31 @@ with_review_timestamp.createOrReplaceTempView("reviews")
 # or
 #result.show()
 
-##Question 3: How many records are in the reviews dataframe? 
+##Question 4: How many records are in the reviews dataframe? 
 result = spark.sql("SELECT count(*) FROM reviews")
 result.show()
 
-##Question 4: Print the first 5 rows of the dataframe. 
+##Question 5: Print the first 5 rows of the dataframe. 
 ##Some of the columns are long - print the entire record, regardless of length.
 result = spark.sql("SELECT * FROM reviews LIMIT 5")
 result.show(truncate=False)
 
-##Question 5: Create a new dataframe based on "reviews" with exactly 1 column: the value of the product category field.
+##Question 6: Create a new dataframe based on "reviews" with exactly 1 column: the value of the product category field.
 ##Look at the first 50 rows of that dataframe. 
 ##Which value appears to be the most common?
 just_product_category = spark.sql("SELECT product_category from reviews LIMIT 50")
 just_product_category.show(n=50)
 
-##Question 6: Find the most helpful review in the dataframe - the one with the highest number of helpful votes.
+##Question 7: Find the most helpful review in the dataframe - the one with the highest number of helpful votes.
 #What is the product title for that review? How many helpful votes did it have?
 most_helpful = spark.sql("SELECT product_title, helpful_votes from reviews ORDER BY helpful_votes desc LIMIT 1")
 most_helpful.show(truncate=False)
 
-##Question 7: How many reviews exist in the dataframe with a 5 star rating?
+##Question 8: How many reviews exist in the dataframe with a 5 star rating?
 five_star_reviews = spark.sql("SELECT count(*) from reviews where star_rating = \"5\"")
 five_star_reviews.show()
 
-##Question 8: Find the date with the most purchases.
+##Question 9: Find the date with the most purchases.
 ##Print the date and total count of the date which had the most purchases.
 purchase_date_and_count = spark.sql("SELECT purchase_date, count(*) from reviews GROUP BY purchase_date ORDER BY count(*) DESC LIMIT 1")
 purchase_date_and_count.show()
--- week3_query_with_python_api/week3_query_with_python_api.py
+++ week3_query_with_python_api/week3_query_with_python_api.py
@@ -1,10 +1,26 @@
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import desc, col
 from pyspark.sql.functions import current_timestamp
 
 
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+aws_session_token = os.environ.get("AWS_SESSION_TOKEN")
+
 # Create a SparkSession
-spark = SparkSession.builder.appName("Week2Lab").getOrCreate()
+spark = SparkSession.builder \
+    .appName("Week3Lab") \
+    .config("spark.sql.shuffle.partitions", "3") \
+    .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider') \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
+    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
+    .config("spark.hadoop.fs.s3a.session.token", aws_session_token) \
+    .config("spark.sql.catalogImplementation", "hive") \
+    .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3,org.apache.hadoop:hadoop-aws:3.2.0,com.amazonaws:aws-java-sdk-bundle:1.11.375') \
+    .enableHiveSupport()\
+    .getOrCreate()
 
 #Question 1: Read the tab separated file named "resources/reviews.tsv.gz" into a dataframe.
 #You will use the "reviews" dataframe defined here to answer all the questions below...
@@ -53,6 +69,19 @@ print(purchase_date_and_count.first())
 #Print the schema and inspect a few rows of data to make sure the data is correctly populated.
 with_review_timestamp = reviews.withColumn("review_timestamp", current_timestamp())
 with_review_timestamp.printSchema()
+
+#Question 10: Write the dataframe with load timestamp to s3a://hwe-HANDLE/bronze/reviews in Parquet format.
+with_review_timestamp.write \
+   .mode("overwrite") \
+   .parquet("s3a://hwe-tsagona/bronze/reviews/")
+
+#Question 11: Read the tab separated file named "resources/customers.tsv.gz" into a dataframe
+#Write to S3 under s3a://BUCKET/path/bronze/customers
+#You will use this "customers" data set in a later lab...
+customers = spark.read.csv("resources/customers.tsv.gz", sep="\t", header=True)
+customers.write \
+    .mode("overwrite") \
+    .parquet("s3a://hwe-tsagona/bronze/customers")
 
 # Stop the SparkSession
 spark.stop()
