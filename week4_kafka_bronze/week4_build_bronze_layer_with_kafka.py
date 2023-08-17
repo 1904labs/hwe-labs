5e8ba2dfe5ef9a8d51f457f29e8140153faf3e06
Tim Sagona <tsagona@1904labs.com>
2023-08-16 20:54:15 -0500

    Cleaning up files for week 4 lab

--- week4_kafka_bronze/week4_build_bronze_layer_with_kafka.py
+++ week4_kafka_bronze/week4_build_bronze_layer_with_kafka.py
@@ -16,7 +16,7 @@ password = os.environ.get("HWE_PASSWORD")
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 aws_session_token = os.environ.get("AWS_SESSION_TOKEN")
-kafka_topic = "timsagona"
+kafka_topic = "reviews"
 
 # Create a SparkSession
 spark = SparkSession.builder \
