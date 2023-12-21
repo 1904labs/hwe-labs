55f7ab16ee1d05d9194b74735c222fbae6cedebf
nickhepp <nickhepp@gmail.com>
2023-12-20 23:04:25 -0600

    Working on the stretch challenge

--- week2/test_week2_lab.py
+++ week2/test_week2_lab.py
@@ -3,20 +3,32 @@ from pyspark.sql.types import StructType, StructField, StringType
 import pandas as pd
 from week2_lab import get_most_frequent_product_categories
 
+#def get_test_schema():
+#    return StructType([
+#        StructField("color", StringType(), True),
+#        StructField("product_category", StringType(), True),
+#        StructField("title", StringType(), True)
+#    ])
+
+#def get_expected_schema():
+#    return StructType([
+#        StructField("product_category", StringType(), True),
+#    ])
+
+
 def get_most_frequent_product_categories__one_most_frequent__most_frequent_returned():
     ### ARRANGE
-    spark = SparkSession.builder.appName("Testing get_most_frequent_product_categories").getOrCreate()
-
     sample_data = [{"color": "yellow", "product_category": "movies", "title": "Mary Poppins"},
             {"color": "blue", "product_category": "board games", "title": "Monopoly"},
             {"color": "green", "product_category": "video games", "title": "Halo"},
             {"color": "red", "product_category": "board games", "title": "Risk"}]
     df = spark.createDataFrame(sample_data)
-    expected_df = spark.createDataFrame(data=[("board games")], schema=["product_category"])
+
+    expected_data = [{"product_category": "board games"}]
+    expected_df = spark.createDataFrame(data=expected_data)
     
     ### ACT
     result_df: DataFrame = get_most_frequent_product_categories(df)
-    spark.stop()
 
     ### ASSERT
     pd.testing.assert_frame_equal(result_df.toPandas(), expected_df.toPandas())
@@ -24,8 +36,6 @@ def get_most_frequent_product_categories__one_most_frequent__most_frequent_retur
 
 def get_most_frequent_product_categories__two_most_frequent__two_frequent_returned():
     ### ARRANGE
-    spark = SparkSession.builder.appName("Testing get_most_frequent_product_categories").getOrCreate()
-
     sample_data = [{"color": "yellow", "product_category": "movies", "title": "Mary Poppins"},
             {"color": "blue", "product_category": "board games", "title": "Monopoly"},
             {"color": "green", "product_category": "video games", "title": "Halo"},
@@ -37,7 +47,6 @@ def get_most_frequent_product_categories__two_most_frequent__two_frequent_return
 
     ### ACT
     result_df = get_most_frequent_product_categories(df)
-    spark.stop()
 
     ### ASSERT
     pd.testing.assert_frame_equal(result_df.toPandas(), expected_df.toPandas())
@@ -45,8 +54,6 @@ def get_most_frequent_product_categories__two_most_frequent__two_frequent_return
 
 def get_most_frequent_product_categories__no_rows__none_returned():
     ### ARRANGE
-    spark = SparkSession.builder.appName("Testing get_most_frequent_product_categories").getOrCreate()
-
     # define the schema, but include no data
     schema = StructType([
         StructField("color", StringType(), True),
@@ -56,16 +63,17 @@ def get_most_frequent_product_categories__no_rows__none_returned():
     df = spark.createDataFrame([], schema)
 
     ### ACT
-    result: [] = get_most_frequent_product_categories(df)
-    spark.stop()
+    result = get_most_frequent_product_categories(df)
 
     ### ASSERT
     assert result is None
 
 
 
+spark = SparkSession.builder.appName("Testing get_most_frequent_product_categories").getOrCreate()
 
 get_most_frequent_product_categories__one_most_frequent__most_frequent_returned()
 get_most_frequent_product_categories__two_most_frequent__two_frequent_returned()
 get_most_frequent_product_categories__no_rows__none_returned()
 
+spark.stop()
